c245a4b2a8980c26e827fbe78c73e2163eab1c31
Alex <lex.courtney18@gmail.com>
2022-09-24 09:48:27 -0500

    correct thought-routes for routes using userid/thought/id

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -17,7 +17,7 @@ const thoughtController = {
     },
 
     getThoughtById({ params }, res) {
-        Thought.findOne({ _id: params.id })
+        Thought.findOne({ _id: params.thoughtId })
             .populate({
                 path: 'reactions',
                 select: '-__v'
@@ -56,7 +56,7 @@ const thoughtController = {
     },
 
     updateThought({ params, body }, res) {
-        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        Thought.findOneAndUpdate({ _id: params.thoughtId }, body, { new: true, runValidators: true })
             .then(dbThoughtData => {
                 if (!dbThoughtData) {
                     res.status(404).json({ message: 'No thought found with this id!' });
@@ -68,7 +68,7 @@ const thoughtController = {
     },
 
     deleteThought({ params }, res) {
-        Thought.findOneAndDelete({ _id: params.id })
+        Thought.findOneAndDelete({ _id: params.thoughtId })
             .then(dbThoughtData => {
                 if (!dbThoughtData) {
                     res.status(404).json({ message: 'No thought found with this id!' });
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -9,7 +9,7 @@ router
 
 
 router
-    .route('/:userId/:id')
+    .route('/:userId/:thoughtId')
     .get(getThoughtById)
     .put(updateThought)
     .put(addReaction)
